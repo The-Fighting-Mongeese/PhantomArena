87c4534e8509c56ac3aede35eb896386f7a95d36
Jason Cheung <jayson.h.cheung@gmail.com>
2017-09-27 23:17:25 -0700

    Changing phase in environment will kill player
    Phasing into a wall or ground will kill the player.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,10 @@ public class PlayerController : NetworkBehaviour {
     private float speed = 10.0f;
 
     [SerializeField]
-    private PlayerCore core;
+    float coreHeight = 0.5f;    // from center to top / bottom (actually half height than) 
+
+    [SerializeField]
+    float coreRadius = 0.3f;
 
     private Rigidbody rb;
 
@@ -21,77 +24,42 @@ public class PlayerController : NetworkBehaviour {
         rb = GetComponent<Rigidbody>();
         phantomLayer = LayerMask.NameToLayer("Phantom");
         physicalLayer = LayerMask.NameToLayer("Physical");
-        // transform.SetAllLayers(CombineLayers("Phantom", "Physical"));
-        var foo = LayerMask.GetMask("Phantom", "Physical");
-        Debug.Log(foo);
-        transform.SetAllLayers(foo);
-        Debug.Log(test.value);
     }
 
-    float coreHeight = 0.8f;    // from center to top / bottom (actually half height than) 
-    float coreRadius = 0.3f;
-
-
-    void Update () {
+    void Update ()
+    {
         // Handle movement 
         //if (!isLocalPlayer) return;
         float deltaX = Input.GetAxis("Horizontal") * speed;
         float deltaZ = Input.GetAxis("Vertical") * speed;
         Vector3 movement = new Vector3(deltaX, 0, deltaZ);
-        movement = Vector3.ClampMagnitude(movement, speed); //limits diagonal movement to the same speed as movement along an axis
+        movement = Vector3.ClampMagnitude(movement, speed);     // limits diagonal movement to the same speed as movement along an axis
         movement *= Time.deltaTime;
-        // rb.velocity = movement;
-        transform.Translate(movement);
+        transform.Translate(movement);                          // TODO: Change to force or velocity changing, or move to FixedUpdate to prevent going into walls. 
 
-
-
-        // Warning! Not 100% sure if this mirrors exactly what Physics.CheckCapsule is
-        // DebugExtension.DebugCapsule(transform.position + transform.up * coreHeight / 2, transform.position + transform.up * -coreHeight, coreRadius);
-        
         // Handle phase change 
         if (Input.GetKeyDown(KeyCode.F))
         {
-            if (Physics.CheckCapsule(transform.position + transform.up * coreHeight / 2, transform.position + transform.up * -coreHeight / 2, coreRadius))
+            // Check if player is phasing into environment
+            var objectsInCore = Physics.OverlapCapsule(
+                transform.position + transform.up * coreHeight / 2, 
+                transform.position + transform.up * -coreHeight / 2, 
+                coreRadius);
+
+            if (objectsInCore.Length > 1)  // there will always be one for the player
             {
-                // always in something ! (player)
-                Debug.Log("INSIDE SOMETHING");
+                Debug.Log("Player is phasing into something.");
+                // TODO: Kill player
+                CmdRespawn();
+                return;
             }
-            return;
-            // If core is inside something, player is currently phasing through environment. 
-            if (core.CoreInside)
-            {
-                // Die - phased into environment. 
-                CmdRespawn();   
-            }
-            CmdRespawn();
+            
+            // TODO: Check phase change cooldown / resource use 
+            
+            // Player is not phasing into environment, allow phase change.
             transform.SetAllLayers((gameObject.layer == physicalLayer) ? phantomLayer : physicalLayer);
         }
 	}
-
-    public LayerMask test;
-
-    int CombineLayers(params string[] layers)
-    {
-        int finalLayerMask = 0;
-        foreach (string s in layers)
-        {
-            var layer = LayerMask.NameToLayer(s);
-
-            Debug.Log(layer);
-            finalLayerMask = finalLayerMask | layer;
-            Debug.Log(finalLayerMask);
-        }
-
-        Debug.Log(finalLayerMask);
-        Debug.Log(test.value);
-
-        return finalLayerMask;
-    }
-
-    void OnDrawGizmosSelected()
-    {
-        DebugExtension.DrawCapsule(transform.position + (transform.up * coreHeight), transform.position - (transform.up * coreHeight), coreRadius);
-    }
 
     [Command]
     public void CmdRespawn()
@@ -104,4 +72,23 @@ public class PlayerController : NetworkBehaviour {
     {
         transform.position = Vector3.zero;
     }
+
+    void OnDrawGizmosSelected()
+    {
+        // Drawing core bounds (Note: the calculations are correct, do not use half coreHeight)
+        DebugExtension.DrawCapsule(transform.position + (transform.up * coreHeight), transform.position - (transform.up * coreHeight), coreRadius);
+    }
 }
+
+/* Function to combine layers. 
+int CombineLayers(params string[] layers)
+{
+    int finalLayerMask = 0;
+    foreach (string s in layers)
+    {
+        var layer = LayerMask.NameToLayer(s);
+        finalLayerMask = finalLayerMask | layer;
+    }
+    return finalLayerMask;
+}
+*/
