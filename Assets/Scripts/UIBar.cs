696be3b654eafd7fa49d8a315e38c95c2aacb082
Rian Neufelt <RNeufelt@live.ca>
2017-10-04 14:03:30 -0700

    Integrated UIBars into main UI.

---
+++ Assets/Scripts/UIBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBar : MonoBehaviour
+{
+    public RectTransform Foreground;
+    public Text UIText;
+    public int Max = 100;
+
+
+    public void UpdateUI(int cur)
+    {
+        Foreground.localScale = new Vector3(cur/Max, 1, 1);
+        UIText.text = cur + " / " + Max;
+    }
+
+    // Use this for initialization
+    void Awake ()
+    {
+        Foreground.localScale = new Vector3(1, 1, 1);
+        UIText.text = Max + " / " + Max;
+    }
+	
+}
