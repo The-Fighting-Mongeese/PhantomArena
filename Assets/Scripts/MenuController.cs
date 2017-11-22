af0686a54c8cf6f80568b03c08e41548c703acf0
limbermuffin <limbermuffin@gmail.com>
2017-11-21 21:08:34 -0800

    Added menu scene + menu controller

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Button1OnClick() {
+        SceneManager.LoadScene("DanielTerrainScene");
+    }
+    public void Button2OnClick() {
+        SceneManager.LoadScene("JasonWorkSpace");
+    }
+    public void Button3OnClick() {
+        SceneManager.LoadScene("MarkusWorkSpace");
+    }
+}
