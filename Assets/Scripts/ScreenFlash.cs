71f8bd77be11db92d396012049064e3ce8b50e52
Daniel Tian <daniel_tian2002@hotmail.com>
2017-12-12 18:04:38 -0800

    synched branch with markusbranch

--- Assets/Scripts/ScreenFlash.cs
+++ Assets/Scripts/ScreenFlash.cs
@@ -15,11 +15,7 @@ public class ScreenFlash : MonoBehaviour {
     public static IEnumerator FlashScreen(int numOfFlashes, float timeBetweenEachFlash)
     {
 
-        print("flas screen");
-
         int i = 0;
-
-        //screenFlashUi.SetActive(true);
 
         while (i < numOfFlashes)
         {
@@ -31,7 +27,6 @@ public class ScreenFlash : MonoBehaviour {
             screenFlashUi.GetComponent<Image>().color = new Color(0,0,0,0);
 
             yield return new WaitForSeconds(timeBetweenEachFlash);
-            print("flas asdfasdf");
             i++;
         }
     }
