8843d770028d32ff5e5c6e31c3aab510a70795ca
Daniel Tian <daniel_tian2002@hotmail.com>
2017-12-12 17:41:50 -0800

    added screen flash, as well as removed overhealing

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -48,7 +48,11 @@ public class Health : NetworkBehaviour
     {
         if (!isServer) return;  
         currentHealth += amountHealed;
-        
+        if (currentHealth >= maxHealth) //prevents overheal
+        {
+            currentHealth = maxHealth;
+            return;
+        }
     }
 
     [Command]
@@ -63,6 +67,9 @@ public class Health : NetworkBehaviour
     public void RpcHeal(int amountHealed)
     {
         currentHealth += amountHealed;
+        if (currentHealth >= maxHealth) //prevents overheal
+            currentHealth = maxHealth;
+        
         hpText.text = currentHealth + "/" + maxHealth;
         print(GetComponent<Chat>().pName + " rpc heal called");
     }
@@ -72,6 +79,8 @@ public class Health : NetworkBehaviour
     {
         Debug.Log("Current life " + currentHealth + " amount " + amount);
         if (!alive) return;
+
+        RpcFlashScreen();
 
         currentHealth -= amount;    // syncvar - does not require Rpc call
         if (currentHealth <= 0)
@@ -105,6 +114,8 @@ public class Health : NetworkBehaviour
 
         if (hitVfx != null)         // play hit vfx if it has one
             hitVfx.Play();
+
+        RpcFlashScreen();
 
         currentHealth -= amount;    // syncvar - does not require Rpc call
         if (currentHealth <= 0)
@@ -145,6 +156,12 @@ public class Health : NetworkBehaviour
         }
     }
 
+    [ClientRpc] 
+    void RpcFlashScreen()
+    {
+        if(isLocalPlayer)
+        StartCoroutine(ScreenFlash.FlashScreen(6, 0.08f));
+    }
 
     [Command]
     void CmdRespawn()
---
+++ Assets/Scripts/ScreenFlash.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenFlash : MonoBehaviour {
+
+    public static GameObject screenFlashUi;
+
+    private void Awake()
+    {
+        screenFlashUi = GameObject.Find("HealthFlash");
+    }
+
+    public static IEnumerator FlashScreen(int numOfFlashes, float timeBetweenEachFlash)
+    {
+
+        print("flas screen");
+
+        int i = 0;
+
+        //screenFlashUi.SetActive(true);
+
+        while (i < numOfFlashes)
+        {
+
+            screenFlashUi.GetComponent<Image>().color = Color.red;
+
+            yield return new WaitForSeconds(timeBetweenEachFlash);
+
+            screenFlashUi.GetComponent<Image>().color = new Color(0,0,0,0);
+
+            yield return new WaitForSeconds(timeBetweenEachFlash);
+            print("flas asdfasdf");
+            i++;
+        }
+    }
+
+}
